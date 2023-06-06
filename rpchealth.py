80d4436a2558b50053b674e7eecff0fed5958480
darkobas <marko@datafund.io>
2023-06-07 01:10:55 +0200

    fix: if block is 0 its down

--- rpchealth.py
+++ rpchealth.py
@@ -100,6 +100,10 @@ async def update_health_status():
                 if server_data['stale_count'][key] >= 3:
                     health_status = 503
 
+            # Handle scenarios where block number is 0 or None
+            if block_number is None or block_number == 0:
+                health_status = 503
+
             server_data['health_status'][key] = health_status
 
             # Log the status change from "UP" to "DOWN" with reason
@@ -107,8 +111,10 @@ async def update_health_status():
                 reason = "Stale Block" if server_data['stale_count'][key] >= 3 else "Unhealthy Status"
                 logger.info(f"Health status for {rpc_address} changed from {old_status} to {health_status} ({reason})")
 
-            # Update server data if the status changes to "UP"
+            # Log the status change from "DOWN" to "UP"
             if health_status == 200 and old_status != 200:
+                logger.info(f"Health status for {rpc_address} changed from {old_status} to {health_status}")
+
                 server_data['stale_count'][key] = 0
                 server_data['last_block'][key] = block_number
 
